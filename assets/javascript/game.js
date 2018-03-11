63972ac2ec1d8c447faf79b9ff6129d3631dc545
Pinelli <npinelli@sop2776.attlocal.net>
2018-03-11 17:08:04 -0400

    changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,56 +1,259 @@
-var game={
-     words:["baseball", "football", "soccer", "lacrosse", "basketball"],
+Main = {};
+Main.WordArray = ["baseball", "football", "basketball", "lacrosse", "soccer"];
+Main.WordUArray = [];
+Main.Lives = 4;
+Main.NumberInBank = Words.length;
+Main.Word = "test";
+Main.WordU = "";
 
-    currentWord=null,
-    lettersOfTheWord: [],
-    wins: 0,
-    lettersGuessed: [],
-    totalGuesses: 0,
-    guessesRemaining: 0,
-    matchedLetters: [],
-    letterGuessed=null,
-
-setUpGame: function (){
-
-},
-
-upDateLettersGuessed: function(){
- 
-},
-
-upDateCurrentWord: function(){
-
-},
-
-upDateGuessesRemaining: function(){
-
-},
-
-upDateWins: function(){
-
-},
-
-restartGame: function(){
-
-},
-
-upDatePage: function(letter){
-    if(this.guessesRemaining === 0){
-        this.restartGame();
-    }
-    else{
-        this.upDateGuesses(letter);
-        this.upDateCurrentWord(letter);
-    }
-},
-
-
-};
-
-game.setUpGame();
-
-document.onkeyup() = function(event) {
-    game.letterGuessed = String.fromCharCode(event.which).toLowerCase();
-    game.upDatePage(game.letterGuessed);
+Main.PullWord = function () {
+    Main.Word = Words.list[(math.floor(math.randon() * main.NumberInBank))];
 }
 
+Main.SetUnderline = function () {
+    Main.PullWord();
+    for (i = 0; i < main.word.length; i++) {
+        Main.WordArray[i] = Main.Word.charAt(i);
+        Main.WordUArray[i] = "_";
+    }
+    Main.WordU = Main.WordUArray.join("");
+    Document.getElementByID("WORD").innerHTML = Main.WordU;
+    document.getElementById("numLetters").innerHTML = Main.Word.length;
+}
+
+Main.UpDateLetter = function (letter) {
+    Main.Changes = 0;
+    for (i = 0; i < main.word.length; i++) {
+        Main.WordArray[i] = Main.Word.charAt(i);
+        if (Main.Word.charAt == letter) {
+            Main.WordUArray[i] = letter;
+            Main.Changes += 1;
+
+        }
+    }
+    if (main.changes < 1) {
+        Main.Lives -= 1;
+        document.getElementById("lives").innerHTML = Main.Lives;
+    }
+    Main.WordU = Main.WordUArray.join("");
+    document.getElementById("WORD").innerHTML = Main.WordU;
+
+    Main.Word1 = Main.WordArray.join("");
+    Main.Word2 = Main.WordArray.join("");
+
+    if (Main.Word1 == Main.Word2) {
+        alert("YOU WIN!  PLAY AGAIN");
+        window.location.reload();
+    }
+    if (Main.Lives < 1) {
+        document.getElementById("WORD").innerHTML == Main.Word1;
+        alert("You Loose, Try Again?");
+        window.location.reload();
+    }
+    $(function() {
+        $("#a").click(function (event) {
+            event.preventDefault();
+            Main.UpdateLetter("a");
+        });
+    }
+    );
+
+    $(function() {
+        $("#b").click(function (event) {
+            event.preventDefault();
+            Main.UpdateLetter("b");
+        });
+    }
+    );
+
+    $(function() {
+        $("#c").click(function (event) {
+            event.preventDefault();
+            Main.UpdateLetter("c");
+        });
+    }
+    );
+
+    $(function() {
+        $("#d").click(function (event) {
+            event.preventDefault();
+            Main.UpdateLetter("d");
+        });
+    }
+    );
+
+    $(function() {
+        $("#e").click(function (event) {
+            event.preventDefault();
+            Main.UpdateLetter("e");
+        });
+    }
+    );
+
+    $(function() {
+        $("#f").click(function (event) {
+            event.preventDefault();
+            Main.UpdateLetter("f");
+        });
+    }
+    );
+
+    $(function() {
+        $("#g").click(function (event) {
+            event.preventDefault();
+            Main.UpdateLetter("g");
+        });
+    }
+    );
+
+    $(function() {
+        $("#h").click(function (event) {
+            event.preventDefault();
+            Main.UpdateLetter("h");
+        });
+    }
+    );
+
+    $(function() {
+        $("#i").click(function (event) {
+            event.preventDefault();
+            Main.UpdateLetter("i");
+        });
+    }
+    );
+
+    $(function() {
+        $("#j").click(function (event) {
+            event.preventDefault();
+            Main.UpdateLetter("j");
+        });
+    }
+    );
+
+    $(function() {
+        $("#k").click(function (event) {
+            event.preventDefault();
+            Main.UpdateLetter("k");
+        });
+    }
+    ); $(function() {
+        $("#l").click(function (event) {
+            event.preventDefault();
+            Main.UpdateLetter("l");
+        });
+    }
+    );
+
+    $(function() {
+        $("#m").click(function (event) {
+            event.preventDefault();
+            Main.UpdateLetter("m");
+        });
+    }
+    );
+
+    $(function() {
+        $("#n").click(function (event) {
+            event.preventDefault();
+            Main.UpdateLetter("n");
+        });
+    }
+    );
+
+    $(function() {
+        $("#o").click(function (event) {
+            event.preventDefault();
+            Main.UpdateLetter("o");
+        });
+    }
+    );
+
+    $(function() {
+        $("#p").click(function (event) {
+            event.preventDefault();
+            Main.UpdateLetter("p");
+        });
+    }
+    );
+
+    $(function() {
+        $("#q").click(function (event) {
+            event.preventDefault();
+            Main.UpdateLetter("q");
+        });
+    }
+    );
+
+    $(function() {
+        $("#r").click(function (event) {
+            event.preventDefault();
+            Main.UpdateLetter("r");
+        });
+    }
+    );
+
+    $(function() {
+        $("#s").click(function (event) {
+            event.preventDefault();
+            Main.UpdateLetter("s");
+        });
+    }
+    );
+
+    $(function() {
+        $("#t").click(function (event) {
+            event.preventDefault();
+            Main.UpdateLetter("t");
+        });
+    }
+    );
+
+    $(function() {
+        $("#u").click(function (event) {
+            event.preventDefault();
+            Main.UpdateLetter("u");
+        });
+    }
+    );
+
+    $(function() {
+        $("#v").click(function (event) {
+            event.preventDefault();
+            Main.UpdateLetter("v");
+        });
+    }
+    ); $(function() {
+        $("#w").click(function (event) {
+            event.preventDefault();
+            Main.UpdateLetter("w");
+        });
+    }
+    );
+
+    $(function() {
+        $("#x").click(function (event) {
+            event.preventDefault();
+            Main.UpdateLetter("x");
+        });
+    }
+    );
+
+    $(function() {
+        $("#y").click(function (event) {
+            event.preventDefault();
+            Main.UpdateLetter("y");
+        });
+    }
+    );
+
+    $(function() {
+        $("#z").click(function (event) {
+            event.preventDefault();
+            Main.UpdateLetter("z");
+        });
+    }
+    );
+}
+
+Main.PullWord();
+Main.SetUnderline();
